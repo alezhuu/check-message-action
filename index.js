33f977e0fa9ccd345cf24d7f9d149a10af2dc492
alezhuu <130742668+alezhuu@users.noreply.github.com>
2023-05-15 16:53:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const main = () => {
-  return new Promise((resolve, reject) => {
-    try {
-      const event = process.env.GITHUB_EVENT_NAME;
-      let message;
+const main = async() => {
+     try {
+    const event = process.env.GITHUB_EVENT_NAME;
+    let message;
 
-      if (event === 'push') {
-        message = core.getInput('push_message') || '';
-      } else if (event === 'pull_request') {
-        message = core.getInput('pull_message') || '';
-      } else {
-        throw new Error(`Unsupported event: ${event}`);
-      }
-
-      if (message.startsWith('#')) {
-        console.log("Message starts with '#'.");
-        core.setOutput('result', 'success');
-        resolve('success');
-      } else {
-        console.log("Message does not start with '#'.");
-        core.setOutput('result', 'failure');
-        resolve('failure');
-      }
-    } catch (error) {
-      core.setFailed(error.message);
-      reject(error);
+    if (event === 'push') {
+      message = core.getInput('push_message');
+    } else if (event === 'pull_request') {
+      message = core.getInput('pull_message');
+    } else {
+      throw new Error(`Unsupported event: ${event}`);
     }
-  });
-};
 
-main()
-  .then((result) => {
-    console.log('Promise resolved:', result);
-  })
-  .catch((error) => {
-    console.error('Promise rejected:', error);
-  });
+    if (message.startsWith('#')) {
+      console.log("Message starts with '#'.");
+      core.setOutput('result', 'success');
+    } else {
+      console.log("Message does not start with '#'.");
+      core.setOutput('result', 'failure');
+      process.exit(1);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
+}
+
+main();
