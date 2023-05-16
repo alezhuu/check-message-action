209205d63b5715ab316ffb484473ac34ae25e0f1
Alexandra <sasha.zhurina@gmail.com>
2023-05-16 12:56:16 +0200

    js functions with streams

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const JSONStream = require('JSONStream');
 
-const main = async() => {
-   try {
-    let  message = core.getInput('pull_message');
-      /*
-    if (message.startsWith('#Esto es un ejemplo') || message.startsWith('# Esto es un ejemplo')) {
-      console.log("Correct message format.");
-      core.setOutput('result', 'success');
-    } else {
-      core.setOutput('result', 'failure');
-      core.setFailed("Incorrect message format.");
-    }*/
-      const keys=Object.keys(message);
-      console.log(keys);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
 
-}
+const main = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        let  pullRequestInfo = core.getInput('pull_info');
+        const parser = JSONStream.parse([true,'commit','message']);
+
+        const stream = fs.createReadStream(pullRequestInfo, { encoding: 'utf8' });
+  
+      stream.pipe(parser);
+  
+      const results = [];
+      parser.on('data', (data) => {
+        results.push(data);
+      });
+  
+      parser.on('end', () => {
+        resolve(results);
+        if (results[results.length-1].startsWith('#Esto es un ejemplo') || results[results.length-1].startsWith('# Esto es un ejemplo')) {
+          console.log("Correct message format.");
+          core.setOutput('result', 'success');
+        } else {
+          core.setOutput('result', 'failure');
+          core.setFailed("Incorrect message format.");
+        }
+      });
+  
+      parser.on('error', (err) => {
+        reject(err);
+      });
+    
+      } catch (error) {
+        core.setFailed(error.message);
+      }
+    });
+  };
+  
+
+
 
 main();
