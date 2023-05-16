d5e32082672255d932ed07f3848096a5b37ee768
alezhuu <130742668+alezhuu@users.noreply.github.com>
2023-05-16 12:03:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 const main = async() => {
    try {
     let  message = core.getInput('pull_message');
+      /*
     if (message.startsWith('#Esto es un ejemplo') || message.startsWith('# Esto es un ejemplo')) {
       console.log("Correct message format.");
       core.setOutput('result', 'success');
     } else {
       core.setOutput('result', 'failure');
       core.setFailed("Incorrect message format.");
-    }
+    }*/
+      
+      console.log(JSON.stringify(message));
   } catch (error) {
     core.setFailed(error.message);
   }
