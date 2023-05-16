5de0f588b0d5951c6b7fe0a731dc2005f8a90314
alezhuu <130742668+alezhuu@users.noreply.github.com>
2023-05-16 14:39:46 +0200

    BACK to easy-Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,22 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
-const JSONStream = require('JSONStream');
 
+const main = async() => {
+   try {
+    let  message = core.getInput('pull_info');
+         console.log(message);
+    if (message.startsWith('#Esto es un ejemplo') || message.startsWith('# Esto es un ejemplo')) {
+      console.log("Correct message format.");
+      core.setOutput('result', 'success');
+    } else {
+      core.setOutput('result', 'failure');
+      core.setFailed("Incorrect message format.");
+    }
+     
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 
-const main = () => {
-    return new Promise((resolve, reject) => {
-      try {
-        let  pullRequestInfo = core.getInput('pull_info');
-        const parser = JSONStream.parse([true,'commit','message']);
-        console.log(typeof(pullRequestInfo));
-        console.log(pullRequestInfo);
-
-        /*
-        const stream = fs.createReadStream('C:/Users/alzuhira/Desktop/Spectral/informed-domains-v2.json', { encoding: 'utf8' });
-  
-      stream.pipe(parser);
-  
-      const results = [];
-      parser.on('data', (data) => {
-        results.push(data);
-      });
-  
-      parser.on('end', () => {
-        resolve(results);
-        if (results[results.length-1].startsWith('#Esto es un ejemplo') || results[results.length-1].startsWith('# Esto es un ejemplo')) {
-          console.log("Correct message format.");
-          core.setOutput('result', 'success');
-        } else {
-          core.setOutput('result', 'failure');
-          core.setFailed("Incorrect message format.");
-        }
-      });
-  
-      parser.on('error', (err) => {
-        reject(err);
-      });
-    */
-      } catch (error) {
-        core.setFailed(error.message);
-      }
-    });
-  };
-  
-
-
+}
 
 main();
