d76e26b59fdb6666fe9c22e63d85c59de310ad4f
alezhuu <130742668+alezhuu@users.noreply.github.com>
2023-05-17 15:41:12 +0200

    Working version - commented index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const github = require('@actions/github');
 
 const main = async() => {
    try {
-    let  message = core.getInput('pull_info');
-    if (message.startsWith('#Esto es un ejemplo') || message.startsWith('# Esto es un ejemplo')) {
+    let  message = core.getInput('message'); //Get the message passed to the action
+      //Check if the message has established format or not
+    if (message.startsWith('#Esto es un ejemplo')) {
       console.log("Correct message format.");
       core.setOutput('result', 'success');
     } else {
@@ -13,7 +14,7 @@ const main = async() => {
     }
      
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message); //Catching any errors
   }
 
 }
